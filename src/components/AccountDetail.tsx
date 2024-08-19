58351e733c6e658032cdcf12e887a7824d5a7c2f
JI-Villalobos <chuy12villalobos@gmail.com>
2024-08-19 12:44:53 -0600

    update account details page

--- src/components/AccountDetail.tsx
+++ src/components/AccountDetail.tsx
@@ -1,4 +1,7 @@
 import { Accounting } from "@/services/api/accounts"
+import { BiArrowBack, BiLockOpen } from "react-icons/bi";
+import Image from "next/image"
+import Link from "next/link";
 
 type Props = {
   seller: string,
@@ -10,18 +13,28 @@ export default function Details({ seller, accounting }: Props): JSX.Element {
     <>
       {
         accounting ? (
-          <div className="rounded border border-mp-light-green bg-mp-gray-soft mt-6 w-8/12 flex flex-col items-center">
-            <p className="font-coda text-mp-dark text-2xl mt-2">Detalle de corte número: <span className="text-mp-blue">{accounting.id}</span></p>
-            <p className="font-coda text-mp-dark text-xl mt-2">Timestamp: <span className="text-mp-blue">{accounting.date}</span></p>
-            <p className="font-coda text-mp-dark text-xl mt-2">Vendedora: <span className="text-mp-blue">{seller}</span></p>
-            <p className="font-coda text-mp-green text-xl mt-2">Ingresos:</p>
+          <div className="rounded shadow-lg mt-6 w-1/3 flex flex-col items-center">
+            <div className="flex flex-row justify-center w-8/12">
+              <Image src="/mp_logo.png" width={70} height={45} alt='mp logo' className='m-2' />
+              
+            </div>
+            <p className="text-mp-dark text-2xl w-10/12 text-center m-6 border-b-2 border-mp-strong-gray">Detalle de corte número: <span className="text-mp-blue">{accounting.id}</span></p>
+            <p className="text-mp-dark text-xl mt-2">Timestamp: <span className="text-mp-blue">{accounting.date}</span></p>
+            <p className="text-mp-dark text-xl mt-2">Vendedora: <span className="text-mp-blue">{seller}</span></p>
+            <span className="mt-4 border-b-2 border-mp-strong-gray w-1/2"></span>
+            <p className="text-mp-green text-xl font-bold">Ingresos:</p>
             {
-              accounting.incomeRegistries?.map(income => (<p key={`income-id${income.id}`} className="font-coda text-mp-dark mt-1">{income.tag}: ${income.amount}</p>))
+              accounting.incomeRegistries?.map(income => (<p key={`income-id${income.id}`} className="text-mp-dark mt-1">{income.tag}: ${income.amount}</p>))
             }
-            <p className="font-coda text-mp-green text-xl mt-2" >Gastos:</p>
+            <span className="mt-4 border-b-2 border-mp-strong-gray w-1/2"></span>
+            <p className="text-mp-green text-xl mt-2 font-bold" >Gastos:</p>
             {
-              accounting.expenseRegistries?.map(expense => (<p key={`income-id${expense.id}`} className="font-coda text-mp-dark m-1">{expense.description}: ${expense.amount}</p>))
+              accounting.expenseRegistries?.map(expense => (<p key={`income-id${expense.id}`} className="text-mp-dark mb-4">{expense.description}: ${expense.amount}</p>))
             }
+            <Link href={"/"} className="m-6 text-mp-blue font-semibold flex flex-row items-center">
+              <BiArrowBack/>
+              Regresar
+            </Link>
           </div>)
           : <p></p>
       }
