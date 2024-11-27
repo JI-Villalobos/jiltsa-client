3ddff27cf223c25a0f5bb7e65e09050a454624ea
JI-Villalobos <chuy12villalobos@gmail.com>
2024-11-27 17:45:54 -0600

    update version label

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -19,7 +19,7 @@ export default function Header(): JSX.Element {
       <a href="https://checker-ear-0d6.notion.site/JILTSA-Manual-de-usuario-5382089db632443897a40d864dc3a903?pvs=4" target="_blank">
         <div className="flex flex-row justify-center items-center">
           <Image src="/handbook.svg" width={25} height={25} alt='mp logo' className='m-2' />
-          <p className="text-mp-dark text-sm">Versión 0.8.1</p>
+          <p className="text-mp-dark text-sm">Versión 0.8.2</p>
         </div>
       </a>
       <DemoMode />
