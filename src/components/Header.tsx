3c73dbf5e6f8c5449576f3f3111afeaa2e142693
JI-Villalobos <chuy12villalobos@gmail.com>
2024-12-05 14:48:36 -0600

    update christmass logos

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,10 +14,10 @@ export default function Header(): JSX.Element {
 
   return (
     <div className='flex flex-row bg-mp-gray-soft items-center justify-between w-full'>
-      <Image src="/mp_logo.png" width={60} height={35} alt='mp logo' className='m-2' />
+      <Image src="/chrst-logo.png" width={80} height={45} alt='mp logo' className='m-2' />
       <a href="https://checker-ear-0d6.notion.site/JILTSA-Manual-de-usuario-5382089db632443897a40d864dc3a903?pvs=4" target="_blank">
         <div className="flex flex-row justify-center items-center">
-          <Image src="/handbook.svg" width={25} height={25} alt='mp logo' className='m-2' />
+          <Image src="/christmas-tree.svg" width={30} height={30} alt='mp logo' className='m-2' />
           <p className="text-mp-dark text-sm">Versión 0.8.4</p>
         </div>
       </a>
