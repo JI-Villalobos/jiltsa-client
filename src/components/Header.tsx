eb6d4d19afdf06d9b7b71a092badd65090c534ff
JI-Villalobos <chuy12villalobos@gmail.com>
2025-04-01 14:06:37 -0600

    update version labels

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,7 +18,7 @@ export default function Header(): JSX.Element {
       <a href="https://checker-ear-0d6.notion.site/JILTSA-Manual-de-usuario-5382089db632443897a40d864dc3a903?pvs=4" target="_blank">
         <div className="flex flex-row justify-center items-center">
           <BiCode size={20}/>
-          <p className="text-mp-dark text-sm">Versión 0.9.0</p>
+          <p className="text-mp-dark text-sm">Versión 0.9.1</p>
         </div>
       </a>
       <button onClick={handleLogout}>
