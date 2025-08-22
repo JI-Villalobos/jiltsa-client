eb952670fce114870d39bec0d6c4cf35047f4415
JI-Villalobos <chuy12villalobos@gmail.com>
2025-08-21 18:46:05 -0600

    add account-detail modal

--- src/app/components/RowReport.tsx
+++ src/app/components/RowReport.tsx
@@ -8,12 +8,14 @@ import { Accounting } from "../services/api/accounts"
 import { failedRequest, initialStatus, pendingRequest, RequestStatus, successfullRequest } from "../services"
 import { getSeller } from "../services/api/sellers"
 import { useRouter } from "next/navigation"
+import Modal from "./shared/Modal"
 
 type Props = {
   account: Accounting
 }
 
 export default function RowReport({ account }: Props): JSX.Element {
+  const [showModal, setShowModal] = useState(false)
   const [seller, setSeller] = useState<string>('')
   const [status, setStatus] = useState<RequestStatus>(initialStatus)
   const [rowData, setRowData] = useState<RowData>({
@@ -36,15 +38,78 @@ export default function RowReport({ account }: Props): JSX.Element {
   }, [])
 
   return (
-    <tr className="text-center hover:bg-mp-strong-gray hover:cursor-pointer" onClick={() => route.push(`/account-details/${seller}/${account.id}`)}>
-      <td className='whitespace-nowrap px-4 py-2 text-mp-soft-dark text-sm'>{DateFormat(account.date!)}</td>
-      <td className='whitespace-nowrap px-4 py-2 text-mp-dark text-sm'>
-        {status.onLoading ? <Spinner /> : `${seller}`}
-      </td>
-      <td className='whitespace-nowrap px-4 py-2 text-mp-blue text-sm'>{rowData.otherIncomes}</td>
-      <td className='whitespace-nowrap px-4 py-2 text-mp-blue text-sm'>{rowData.incomes}</td>
-      <td className='whitespace-nowrap px-4 py-2 text-mp-green text-sm'>{rowData.total}</td>
-      <td className='whitespace-nowrap px-4 py-2 text-mp-blue text-sm'>{rowData.expenses}</td>
-    </tr>
+    <>
+      <tr className="text-center hover:bg-mp-strong-gray hover:cursor-pointer" onClick={() => setShowModal(true)}>
+        <td className='whitespace-nowrap px-4 py-2 text-mp-soft-dark text-sm'>{DateFormat(account.date!)}</td>
+        <td className='whitespace-nowrap px-4 py-2 text-mp-dark text-sm'>
+          {status.onLoading ? <Spinner /> : `${seller}`}
+        </td>
+        <td className='whitespace-nowrap px-4 py-2 text-mp-blue text-sm'>{rowData.otherIncomes}</td>
+        <td className='whitespace-nowrap px-4 py-2 text-mp-blue text-sm'>{rowData.incomes}</td>
+        <td className='whitespace-nowrap px-4 py-2 text-mp-green text-sm'>{rowData.total}</td>
+        <td className='whitespace-nowrap px-4 py-2 text-mp-blue text-sm'>{rowData.expenses}</td>
+      </tr>
+      {
+        showModal &&
+        <Modal onClose={() => setShowModal(false)}>
+          <div className="flex flex-col w-full items-center justify-center">
+            <label htmlFor="timestamp" className="mt-4 flex flex-col w-2/3">
+              <span className="text-sm font-medium text-mp-soft-dark"> Timestamp </span>
+
+              <input
+                type="text"
+                id="timestamp"
+                className="mt-0.5 rounded border-mp-soft-dark shadow-sm sm:text-sm p-2 text-center text-mp-dark w-full"
+                value={account.date}
+                readOnly
+              />
+            </label>
+            <label htmlFor="seller" className="mt-4 flex flex-col w-2/3">
+              <span className="text-sm font-medium text-mp-soft-dark"> Vendedor(a) </span>
+
+              <input
+                type="seller"
+                id="Email"
+                className="mt-0.5 rounded border-mp-soft-dark shadow-sm sm:text-sm p-2 text-center text-mp-dark w-full"
+                value={account.date}
+                readOnly
+              />
+            </label>
+            <div className="w-full flex flex-col items-center justify-center mb-4">
+              {
+                account.incomeRegistries.map((income) => (
+                  <label htmlFor="seller" className="mt-4 flex flex-col w-2/3" key={income.id}>
+                    <span className="text-sm font-medium text-mp-soft-dark"> {income.tag} </span>
+                    <input
+                      type="seller"
+                      id="Email"
+                      className="mt-0.5 rounded border-mp-soft-dark shadow-sm sm:text-sm p-2 text-center text-mp-dark w-full"
+                      value={income.amount}
+                      readOnly
+                    />
+                  </label>
+                ))
+              }
+            </div>
+            <div className="w-full flex flex-col items-center justify-center mb-4">
+              {
+                account.expenseRegistries.map((expense) => (
+                  <label htmlFor="seller" className="mt-4 flex flex-col w-2/3" key={expense.id}>
+                    <span className="text-sm font-medium text-mp-soft-dark"> {expense.description} </span>
+                    <input
+                      type="seller"
+                      id="Email"
+                      className="mt-0.5 rounded border-mp-soft-dark shadow-sm sm:text-sm p-2 text-center text-mp-dark w-full"
+                      value={expense.amount}
+                      readOnly
+                    />
+                  </label>
+                ))
+              }
+            </div>
+          </div>
+        </Modal>
+      }
+    </>
   )
 }
---
+++ src/app/components/shared/Modal.tsx
@@ -0,0 +1,49 @@
+'use client'
+
+import React from "react"
+import ReactDOM from "react-dom"
+
+interface Props {
+    onClose: CallableFunction
+    children: React.ReactNode
+    title?: string 
+}
+
+const Modal = ({ children, onClose, title }: Props) => {
+    const handleCloseClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        e.preventDefault()
+        onClose()
+    }
+
+
+    const modalContent = (
+        <div className="absolute top-0 left-0 w-full min-h-screen flex 
+                        justify-center items-center bg-mp-soft-dark bg-opacity-30 z-50"
+        >
+            <div className="w-4/5 md:w-1/3 xl:full opacity-">
+                <div className="bg-mp-white rounded">
+                    <div className="flex content-end text-sm">
+                        <button 
+                            onClick={handleCloseClick} 
+                            className="bg-mp-blue w-10 h-10 rounded text-mp-white text-xl 
+                                        m-2 hover:bg-mp-green"
+                        >
+                            x
+                        </button>
+                    </div>
+                    <div className="flex flex-col justify-center w-full">
+                        {title && <h1 className="text-cyan-900 text-xl text-center">{title}</h1>}
+                    </div>
+                    <div className="pt-3 w-full flex flex-col items-center">{children}</div>
+                </div>
+            </div>
+        </div>
+    )
+
+    return ReactDOM.createPortal(
+        modalContent,
+        document.getElementById("modal-root")!
+    )
+}
+
+export default Modal
