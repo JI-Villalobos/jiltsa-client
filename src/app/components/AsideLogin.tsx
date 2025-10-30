e96e61142e8bb1ea514362382e4cddf6e185c765
JI-Villalobos <chuy12villalobos@gmail.com>
2025-10-30 10:47:18 -0600

    update aside login

--- src/app/components/AsideLogin.tsx
+++ src/app/components/AsideLogin.tsx
@@ -5,7 +5,7 @@ export default function AsideLogin(): JSX.Element {
     <div className="flex flex-col bg-mp-gray-soft w-1/2 h-screen items-center justify-center">
       <Image src="/secure_login.svg" alt="secure login image" width={448} height={351} />
       <p className="text-2xl text-mp-dark font-coda font-medium text-center">Es necesario Iniciar Sesión para ingresar al portal</p>
-      <p className="text-xs text-mp-dark font-coda">fmipueblito_version: 0.9.2</p>
+      <p className="text-xs text-mp-dark font-coda">fmipueblito_version: 1.0.0</p>
     </div>
   )
 }
