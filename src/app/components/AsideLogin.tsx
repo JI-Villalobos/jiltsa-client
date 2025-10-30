146b25e04f95f8c5e694c1644e02002af72b4e9b
JI-Villalobos <chuy12villalobos@gmail.com>
2025-10-30 14:53:57 -0600

    update login page

--- src/app/components/AsideLogin.tsx
+++ src/app/components/AsideLogin.tsx
@@ -1,11 +1,16 @@
-import Image from "next/image";
-
 export default function AsideLogin(): JSX.Element {
   return (
-    <div className="flex flex-col bg-mp-gray-soft w-1/2 h-screen items-center justify-center">
-      <Image src="/secure_login.svg" alt="secure login image" width={448} height={351} />
-      <p className="text-2xl text-mp-dark font-coda font-medium text-center">Es necesario Iniciar Sesión para ingresar al portal</p>
-      <p className="text-xs text-mp-dark font-coda">fmipueblito_version: 1.0.0</p>
+    <div className="w-1/2 h-screen bg-gradient-to-tl from-mp-blue to-mp-green flex flex-col items-center justify-center"
+    >
+
+      <div className="w-full flex flex-row items-center justify-center">
+        <div className="h-16 w-16 bg-mp-gray-soft opacity-25 rounded m-2 animate-slow-spin transition-transform duration-1000 ease-in-out transform translate-x-1/2"></div>
+        <div className="h-16 w-16 bg-mp-gray-soft opacity-25 rounded animate-slow-spin transition-transform duration-1200 ease-in-out transform -translate-y-1/2"></div>
+        <div className="h-16 w-16 bg-mp-gray-soft opacity-25 rounded m-2 transition-transform duration-1000 ease-in-out"></div>
+        <div className="h-16 w-16 bg-mp-gray-soft opacity-25 rounded m-2 animate-slow-pulse transition-transform duration-1000 ease-in-out transform translate-x-1/2"></div>
+        <div className="h-16 w-16 bg-mp-gray-soft opacity-25 rounded m-2 animate-slow-pulse transition-transform duration-1000 ease-in-out transform translate-x-1/2"></div>
+
+      </div>
     </div>
   )
 }
