775739416a0ba52d379746b87592828f70e0de34
Arche <chuy12villalobos@gmail.com>
2025-11-06 13:47:46 -0600

    add service selection stage component

--- src/app/seller-home/components/NewExpenseModal.tsx
+++ src/app/seller-home/components/NewExpenseModal.tsx
@@ -1,33 +1,9 @@
 'use client'
 
-import { LuDroplets, LuHouse, LuLightbulb } from "react-icons/lu"
-
 export const NewExpenseModal = () => {
   return (
     <>
-      <div className="flex flex-col items-center justify-center w-full">
-        <p className="text-mp-dark">Seleccina el servicio</p>
-        <div className="grid grid-cols-3 gap-2 p-4 mb-6 mt-4">
-          <button
-            className="p-4 flex flex-row items-center text-mp-dark justify-center gap-2 rounded shadow-md hover:bg-gradient-to-r from-mp-green to-mp-blue hover:text-mp-white"
-          >
-            <LuLightbulb className="text-mp-blue" />
-            Luz Elec.
-          </button>
-          <button
-            className="p-4 flex flex-row items-center text-mp-dark justify-center gap-2 rounded shadow-md hover:bg-gradient-to-r from-mp-green to-mp-blue hover:text-mp-white"
-          >
-            <LuDroplets className="text-mp-blue" />
-            Agua
-          </button>
-          <button
-            className="p-4 flex flex-row items-center text-mp-dark justify-center gap-2 rounded shadow-md hover:bg-gradient-to-r from-mp-green to-mp-blue hover:text-mp-white"
-          >
-            <LuHouse className="text-mp-blue" />
-            Renta
-          </button>
-        </div>
-      </div>
+
     </>
   )
 }
