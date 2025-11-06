14d7a2a1c7f594a8002c9c88039ffa37f1314f10
Arche <chuy12villalobos@gmail.com>
2025-11-06 14:05:47 -0600

    create expense registry store

--- src/utils/variables.ts
+++ src/utils/variables.ts
@@ -4,7 +4,7 @@ export enum Role {
 }
 
 export enum Income {
-  PRONTIPAGOS  = 'PRONTIPAGOS',
+  PRONTIPAGOS = 'PRONTIPAGOS',
   MEDICAMENTO = 'MEDICAMENTO'
 }
 
@@ -13,4 +13,11 @@ export const conceptList = ["Cobro con tarjeta", "Cobro por transferencia", "BBV
 export enum Defaults {
   PRONTIPAGOS = 1,
   MEDICAMENTO = 2
+}
+
+export enum ExpenseStages {
+  SELECT_EXPENSE_TYPE = 'SELECT_EXPENSE_TYPE',
+  PROVIDER_SELECTION = 'PROVIDER_SELECTION',
+  SERVICE_SELECTION = 'SERVICE_SELECTION',
+  NEW_EXPENSE_DETAILS = 'NEW_EXPENSE_DETAILS'
 }
