d5c4bb9c56e46242d16b192d070c1a3d01c91101
Arche <chuy12villalobos@gmail.com>
2025-11-06 14:05:47 -0600

    create expense registry store

---
+++ src/app/store/useExpenseRegistryStore.ts
@@ -0,0 +1,30 @@
+import { ExpenseStages } from "@/utils/variables"
+import { CreateExpenseRegistry, ExpenseRegistry } from "../services/api/expenses"
+import { create } from "zustand"
+
+type State = {
+  stage: ExpenseStages
+  expense: CreateExpenseRegistry
+}
+
+type Action = {
+  setStage: (stage: ExpenseStages) => void
+  setExpense: (expense: ExpenseRegistry) => void
+}
+
+const defaultExpense: CreateExpenseRegistry = {
+  amount: 0,
+  accountingId: 0,
+  description: '',
+  expenseTypeId: 0
+}
+
+const useExpenseRegistryStore = create<State & Action>((set) => ({
+  stage: ExpenseStages.SELECT_EXPENSE_TYPE,
+  expense: defaultExpense,
+  setStage: (stage: ExpenseStages) => set(() => ({ stage: stage })),
+  setExpense: (expense: ExpenseRegistry) => set(() => ({ expense: expense }))
+}))
+
+
+
