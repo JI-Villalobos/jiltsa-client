d54fe9b6ba301ec54c0b7dc71819dcedb0fced36
JI-Villalobos <chuy12villalobos@gmail.com>
2026-01-13 17:57:36 -0600

    add income registry modal

--- src/app/components/shared/CloseOperationButton.tsx
+++ src/app/components/shared/CloseOperationButton.tsx
@@ -1,14 +1,32 @@
 'use client'
 
-import Link from "next/link";
+import { useState } from "react";
 import { LuFolderClosed } from "react-icons/lu";
+import Modal from "./Modal";
+import { IncomeRegistryModal } from "@/app/seller-home/components/IncomeRegistryModal";
 
 export default function CloseOperationButton() {
+    const [showCloseOperationModal, setshowCloseOperationModal] = useState(false)
+
     return (
-        <Link href="/close-operation" className="bg-mp-blue flex flex-row p-2 items-center justify-center rounded text-mp-gray-soft text-sm">
-            <LuFolderClosed />
-            <span className="ml-1"></span>
-            Cerrar Turno
-        </Link>
+        <>
+            <button 
+                className="bg-mp-blue flex flex-row p-2 items-center justify-center rounded text-mp-gray-soft text-sm"
+                onClick={() => setshowCloseOperationModal(true)}
+            >
+                <LuFolderClosed />
+                <span className="ml-1"></span>
+                Cerrar Turno
+            </button>
+            {
+                showCloseOperationModal &&
+                <Modal 
+                    onClose={() => setshowCloseOperationModal(false)}
+                    title="Registro de ventas"
+                >
+                    <IncomeRegistryModal />
+                </Modal>
+            }
+        </>
     )
 }
---
+++ src/app/seller-home/components/IncomeRegistryModal.tsx
@@ -0,0 +1,95 @@
+'use client'
+
+import { failedRequest, initialStatus, successfullRequest } from "@/app/services"
+import { CreateIncomeRegistry, createIncomes } from "@/app/services/api/incomes"
+import { getCurrentAccounting } from "@/utils/appStorage"
+import { Defaults, Income } from "@/utils/variables"
+import { useState } from "react"
+import { LuLoader } from "react-icons/lu"
+
+export const IncomeRegistryModal = () => {
+    const [services, setServices] = useState(0)
+    const [products, setProducts] = useState(0)
+    const [total, setTotal] = useState(0)
+    const [incomeStatus, setIncomeStatus] = useState(initialStatus)
+
+
+    const handleIncomeRegistry = async () => {
+        const accounting = getCurrentAccounting()
+        if (accounting) {
+            const id = accounting.accountingId
+            const incomes: CreateIncomeRegistry[] = [
+                {
+                    accountingId: id,
+                    incomeTypeId: Defaults.MEDICAMENTO,
+                    tag: Income.MEDICAMENTO,
+                    amount: products
+                },
+                {
+                    accountingId: id,
+                    incomeTypeId: Defaults.PRONTIPAGOS,
+                    tag: Income.PRONTIPAGOS,
+                    amount: services
+                }
+            ]
+
+            await createIncomes(incomes)
+                .then((result) => {
+                    setIncomeStatus(successfullRequest)
+                })
+                .catch(() => {
+                    setIncomeStatus(failedRequest)
+                })
+        }
+    }
+
+
+    return (
+        <div className="w-full flex flex-col items-center justify-center gap-2">
+            <div className="flex flex-col items-center justify-center w-full mb-2">
+                <label htmlFor="" className="text-mp-dark text-sm">Prontipagos</label>
+                <input
+                    type="number"
+                    className="p-2 w-1/3 shadow-sm rounded text-center text-sm text-mp-green outline-none border-mp-dark border-opacity-30"
+                    onChange={(e) => {
+                        const services: number = parseFloat(e.currentTarget.value)
+                        setServices(services)
+                        setProducts(total - services)
+                    }}
+                />
+            </div>
+            <div className="flex flex-col items-center justify-center w-full mb-2">
+                <label htmlFor="" className="text-mp-dark text-sm">Medicamento</label>
+                <p className="p-2 w-1/3 shadow-sm rounded text-center text-sm text-mp-green outline-none bg-mp-white border-mp-dark border-opacity-30">
+                    {
+                        typeof products === "number" ? products :
+                            0
+                    }
+                </p>
+            </div>
+            <div className="flex flex-col items-center justify-center w-full mb-2">
+                <label htmlFor="" className="text-mp-dark text-sm">Total de ventas</label>
+                <input
+                    type="number"
+                    className="p-2 w-1/3 shadow-sm rounded text-center text-sm text-mp-green outline-none border-mp-dark border-opacity-30"
+                    onChange={(e) => {
+                        const total: number = parseFloat(e.currentTarget.value)
+                        setTotal(total)
+                        setProducts(total - services)
+                    }}
+                />
+            </div>
+            <button 
+                className="mb-4 w-24 rounded shadow flex flex-col items-center justify-center bg-gradient-to-r from-mp-green to-mp-blue p-2 text-mp-white"
+                onClick={handleIncomeRegistry}
+            >
+                {
+                    incomeStatus.onLoading ? <LuLoader className="animate-spin"/> : 'Confirmar'
+                }
+            </button>
+            {
+                incomeStatus.onError && <p className="mb-2 text-sm text-mp-error">No fue posible registrar los ingrsos, intentalo nuevamente</p>
+            }
+        </div>
+    )
+}
