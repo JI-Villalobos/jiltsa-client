001f6aaa4560c48d7b2f3f5dd1837b5b6e8edf08
JI-Villalobos <chuy12villalobos@gmail.com>
2026-01-29 15:44:41 -0600

    fix close-op details

---
+++ src/app/close-operation/components/SummarySkeleton.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+export const SummarySkeleton = () => {
+    return(
+        <div className="mt-10 w-2/3 flex flex-col justify-center items-center h-40 bg-mp-gray-soft animate-pulse">
+
+        </div>
+    )
+}
